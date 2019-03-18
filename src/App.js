f2c8b1cb471fbf2272f71a0c8121188a6b50ed99
henriquenicolas1 <henriquenicolas@outlook.com.br>
2019-03-17 23:25:56 -0300

    Início implementação tabela

--- src/App.js
+++ src/App.js
@@ -2,15 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 import Graficos from './Graficos.js';
 import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import MenuLateralFiltroElementos from './MenuLateralFiltroElementos';
 import MenuLateralFiltroCompostos from './MenuLateralFiltroCompostos';
 import { EstadoElementosTrue } from './const/EstadoElementosTrue.js';
 import { EstadoCompostosTrue } from './const/EstadoCompostosTrue.js';
+import TabelaCeramicas from './TabelaCeramicas';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    width: '100%',
+    backgroundColor: theme.palette.background.paper
+  }
+});
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      value: 0,
       tipoFiltroCategoria: 'elementos',
       tipoFiltroLogico: 'filtroOU',
       filtroElemento: false,
@@ -44,45 +58,75 @@ class App extends Component {
     });
   };
 
-  render() {
-    return (
-      <div className="App">
-        <Button
-          style={{ margin: '10px' }}
-          variant="contained"
-          onClick={this.toggleDrawer('filtroElemento', true)}
-        >
-          Filtrar Gráficos por Elemento
-        </Button>
-        <Button
-          style={{ margin: '10px' }}
-          variant="contained"
-          onClick={this.toggleDrawer('filtroComposto', true)}
-        >
-          Filtrar Gráficos por Compostos
-        </Button>
-        <MenuLateralFiltroElementos
-          toggleDrawer={this.toggleDrawer}
-          open={this.state.filtroElemento}
-          handleChangeState={this.handleChangeStateElementos}
-          listaElementosSemAlteracao={this.state.elementos}
-        />
-        <MenuLateralFiltroCompostos
-          toggleDrawer={this.toggleDrawer}
-          open={this.state.filtroComposto}
-          handleChangeState={this.handleChangeStateCompostos}
-          listaElementosSemAlteracao={this.state.compostos}
-        />
+  handleChangeAba = (event, value) => {
+    this.setState({ value });
+  };
 
-        <Graficos
-          elementosSelecionados={this.state.elementos}
-          compostosSelecionados={this.state.compostos}
-          tipoFiltroCategoria={this.state.tipoFiltroCategoria}
-          tipoFiltroLogico={this.state.tipoFiltroLogico}
-        />
+  render() {
+    const { classes } = this.props;
+    const { value } = this.state;
+    return (
+      <div className='App'>
+        <div className={classes.root}>
+          <AppBar position='static' color='default'>
+            <Tabs
+              value={value}
+              onChange={this.handleChangeAba}
+              indicatorColor='primary'
+              textColor='primary'
+              variant='scrollable'
+              scrollButtons='auto'
+              centered
+            >
+              <Tab label='Gráficos' />
+              <Tab label='Tabela' />
+            </Tabs>
+          </AppBar>
+          {value === 0 && (
+            <div>
+              <Button
+                style={{ margin: '10px' }}
+                variant='contained'
+                onClick={this.toggleDrawer('filtroElemento', true)}
+              >
+                Filtrar Gráficos por Elemento
+              </Button>
+              <Button
+                style={{ margin: '10px' }}
+                variant='contained'
+                onClick={this.toggleDrawer('filtroComposto', true)}
+              >
+                Filtrar Gráficos por Compostos
+              </Button>
+              {/* <a href='./lista-materiais-ceramicos.pdf' download>
+          Click to download
+        </a> */}
+              <MenuLateralFiltroElementos
+                toggleDrawer={this.toggleDrawer}
+                open={this.state.filtroElemento}
+                handleChangeState={this.handleChangeStateElementos}
+                listaElementosSemAlteracao={this.state.elementos}
+              />
+              <MenuLateralFiltroCompostos
+                toggleDrawer={this.toggleDrawer}
+                open={this.state.filtroComposto}
+                handleChangeState={this.handleChangeStateCompostos}
+                listaElementosSemAlteracao={this.state.compostos}
+              />
+
+              <Graficos
+                elementosSelecionados={this.state.elementos}
+                compostosSelecionados={this.state.compostos}
+                tipoFiltroCategoria={this.state.tipoFiltroCategoria}
+                tipoFiltroLogico={this.state.tipoFiltroLogico}
+              />
+            </div>
+          )}
+          {value === 1 && <TabelaCeramicas />}
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
---
+++ src/TabelaCeramicas.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { MateriaisCeramicosDuplicados } from './lib/MateriaisCeramicosDuplicados.js';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 2,
+    overflowX: 'auto'
+  }
+});
+
+function TabelaCeramicas(props) {
+  const { classes } = props;
+
+  return (
+    <Paper className={classes.root}>
+      <Table>
+        <colgroup>
+          <col style={{ width: '10%' }} />
+          <col style={{ width: '9%' }} />
+          <col style={{ width: '9%' }} />
+          <col style={{ width: '9%' }} />
+          <col style={{ width: '9%' }} />
+          <col style={{ width: '9%' }} />
+          <col style={{ width: '9%' }} />
+          <col style={{ width: '9%' }} />
+          <col style={{ width: '9%' }} />
+          <col style={{ width: '9%' }} />
+          <col style={{ width: '9%' }} />
+        </colgroup>
+        <TableHead>
+          <TableRow>
+            <TableCell>Id </TableCell>
+            <TableCell>Material</TableCell>
+            <TableCell>ST (◦C)</TableCell>
+            <TableCell>Estrutura Cristalina</TableCell>
+            <TableCell>𝜀r</TableCell>
+            <TableCell>Qf (GHz)</TableCell>
+            <TableCell>f0 </TableCell>
+            <TableCell>𝜏f </TableCell>
+            <TableCell> Referência </TableCell>
+            <TableCell> Elementos </TableCell>
+            <TableCell> Compostos </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {MateriaisCeramicosDuplicados.map(material => (
+            <TableRow key={material.id}>
+              <TableCell component='th' scope='row'>
+                {material.Id}
+              </TableCell>
+              <TableCell>{material.Material}</TableCell>
+              <TableCell>{material.TemperaturaSintetizacao}</TableCell>
+              <TableCell>{material.EstruturaCristalina}</TableCell>
+              <TableCell>{material.ConstanteDieletrica}</TableCell>
+              <TableCell>{material.FatorQualidade}</TableCell>
+              <TableCell>{material.FrequenciaMedicao}</TableCell>
+              <TableCell>
+                {material.VariacaoTemperaturaFrequenciaRessonante}
+              </TableCell>
+              <TableCell>{material.Referencia}</TableCell>
+              <TableCell>{material.Elementos}</TableCell>
+              <TableCell>{material.Compostos} </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
+  );
+}
+
+TabelaCeramicas.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(TabelaCeramicas);
